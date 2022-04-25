c19cf4fbc574fbb27e0a2c780614731dbd29e557
liyvonne <79174603+liyvonne@users.noreply.github.com>
2022-04-24 20:00:20 -0700

    Add files via upload

--- SD_Theoretical.ino
+++ SD_Theoretical.ino
@@ -7,11 +7,11 @@
 AudioPlaySdWav           playSdWav1;     //xy=115.00000762939453,215.00000953674316
 AudioSynthWaveformSine   sine1;          //xy=130,69
 AudioMixer4              mixer1;         //xy=317,141
-//AudioAnalyzeToneDetect   tone2;          //xy=330.00001525878906,253.00000762939453
+AudioAnalyzeToneDetect   tone2;          //xy=330.00001525878906,253.00000762939453
 AudioAnalyzeToneDetect   tone1;          //xy=524,134
 AudioOutputI2S           i2s1;           //xy=609,210
 AudioConnection          patchCord1(playSdWav1, 0, mixer1, 3);
-//AudioConnection          patchCord2(playSdWav1, 0, tone2, 0);
+AudioConnection          patchCord2(playSdWav1, 0, tone2, 0);
 AudioConnection          patchCord3(sine1, 0, mixer1, 0);
 AudioConnection          patchCord4(mixer1, tone1);
 AudioConnection          patchCord5(mixer1, 0, i2s1, 0);
@@ -20,26 +20,28 @@ AudioControlSGTL5000     sgtl5000_1;     //xy=265,453
 // GUItool: end automatically generated code
 
 float freq;
-float period;
-float amp = -10;
-float samplePeriod;
+float amp;
+const int pMax = 1023;
+int phasedel;
+int state;
 
 void setup() {
   Serial.begin(9600);
   AudioMemory(30);
   sgtl5000_1.enable();
-  sgtl5000_1.volume(0.5);
+  sgtl5000_1.volume(0.8);
   delay(1000);
 
-  freq = 440;
-  tone1.frequency(freq, 4);
-  //set initial frequency to A440 for testing purposes
-  sine1.frequency(freq);
-  sine1.amplitude(0);
- 
-  period = 1/(freq/1000);
-  //Serial.println("period: ");
-  //Serial.print(period);
+  // Get initial potentiometer reading
+  phasedel = analogRead(A0);
+  state = phasedel/(pMax/3);
+
+  Serial.print("Initial potentiometer reading: ");
+  Serial.println(phasedel);
+  Serial.println("Iniital state: ");
+  Serial.println(state);
+
+
 
   if(!(SD.begin(10))){
     while(1){
@@ -47,15 +49,29 @@ void setup() {
       delay(500);
     }
   }
-  
+
+  // set the frequency to be detected
+  stateChange(state); 
 }
 
-//global vars
-elapsedMillis msecs;
+// global vars
+elapsedMicros microsecs;
 
-void loop() {
-  // put your main code here, to run repeatedly:
-    //Serial.println("sampelPeriod: ");
+void loop() {  
+    int tempState = analogRead(A0)/(pMax/3); 
+
+    if (tempState != state)
+    {
+      state = tempState;
+      Serial.print("Set state to ");
+      Serial.println(state);
+
+      stateChange(state); // change the frequency and WAV file being played
+    }
+
+
+
+   /* 
     if(playSdWav1.isPlaying() == false){
       Serial.println("Start playing 1");
       playSdWav1.play("440.WAV");
@@ -70,21 +86,42 @@ void loop() {
       sine1.amplitude(amp);
       delay(10); 
     }
-    
-    samplePeriod = period/2;
-    //Serial.print(samplePeriod);
-    if (msecs > samplePeriod) {   //IMPORTANT - what sampling rate is needed/possible? order of microseconds?
-    //VOLUME & PHASE ALIGNMENT
-    if (tone1.available()) {
-      msecs = 0;
-      float mixedVolume = tone1.read();
-      
-      if (mixedVolume > 0.05) { //continuosly restart the sine wave until a cancelling phase is hit
-        sine1.phase(0);
-        Serial.print("Change  ");
+    */
+
+    if (microsecs > 1) {   // sampling rate
+      //VOLUME & PHASE ALIGNMENT
+      if (tone1.available()) {
+        microsecs = 0;
+        float mixedVolume = tone1.read();
+
+        if (mixedVolume > 0.05) { //continuosly restart the sine wave until a cancelling phase is hit
+          sine1.phase(0);
+          Serial.print("Change  ");
+        }
+
+        Serial.println(mixedVolume);
       }
-      
-      Serial.println(mixedVolume);
-    }
+   }
+}
+
+// readjust frequency and start playing new WAV file
+void stateChange (int state) {
+  if (state == 0) {
+    freq = 440;
+    playSdWav1.play("440.WAV");
   }
+  else if (state == 1) {
+    freq = 1000;
+    playSdWav1.play("1000Hz.WAV");
+  }
+  else if (state == 2) {
+    freq = 2000;
+    playSdWav1.play("2000Hz.WAV");
+  }
+  tone1.frequency(freq, 4);
+  tone2.frequency(freq, 4);
+  while(tone2.available() == false){}
+  amp = tone2.read();
+  sine1.amplitude(amp);
+  sine1.frequency(freq);
 }
